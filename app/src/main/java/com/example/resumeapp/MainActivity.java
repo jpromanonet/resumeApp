f60ede1de94b8f5e7b9d07e22673e247dbbcf977
Juan P. Romano <programming@jpromano.net>
2021-05-02 18:17:56 -0300

    Fragment home

--- app/src/main/java/com/example/resumeapp/MainActivity.java
+++ app/src/main/java/com/example/resumeapp/MainActivity.java
@@ -4,11 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.resumeapp.home.HomeFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // Home fragment
+    
+    void setHomeFragment(){
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, new HomeFragment()).commit();
+
     }
 }
